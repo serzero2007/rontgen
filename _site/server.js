40929b071c3a8902c0bbee7000b320f730f14f08
amonecho <amonecho1@gmail.com>
2013-05-16 20:27:10 +0100

    changed type

--- _site/server.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.configure(function() {
-    app.use(express.static(__dirname));
-});
-
-console.log('listening on ', process.env.PORT || 8080);
-app.listen(process.env.PORT || 8080);
